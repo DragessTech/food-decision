90a37f898db5b3ad08e0bca1aac1ad436d009f8d
DragessTech <39474275+DragessTech@users.noreply.github.com>
2019-03-18 12:43:41 -0700

    Initial add of files

---
+++ main.js
@@ -0,0 +1,21 @@
+var fastFood = [
+  "McDonalds",
+  "Burger King",
+  "KFC",
+  "Wendys",
+  "Chic fil A",
+  "Arbys",
+  "Taco bell",
+  "Starbucks",
+  "Subway",
+  "Carl Jr/Hardee",
+  "In and Out",
+  "Chipotle",
+  "Sonic Drive-In",
+  "Jack in the Box",
+  "Dairy Queen"
+];
+fastFood.sort();
+var randomAnswer = fastFood[Math.floor(Math.random() * fastFood.length)];
+
+//console.log(randomAnswer)
